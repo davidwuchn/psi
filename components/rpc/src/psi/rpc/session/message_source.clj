ddf659059d183e272941f2ee52e28dae084f908c
Hugo Duncan <hugo@hugoduncan.org>
2026-04-07 09:42:04 -0400

    ⚒ rpc: remove redundant message-source alias

--- components/rpc/src/psi/rpc/session/message_source.clj
+++
@@ -1,8 +0,0 @@
-(ns psi.rpc.session.message-source
-  "Backward-compatible alias for the canonical app-runtime transcript source helpers."
-  (:require
-   [psi.app-runtime.messages :as app-messages]))
-
-(defn session-messages
-  [ctx session-id]
-  (app-messages/session-messages ctx session-id))
