bce29606cfee5a497d73881929edc92ceee49be8
Hugo Duncan <hugo@hugoduncan.org>
2026-04-09 15:03:50 -0400

    ⚒ Harden rpc tests against repo .psi/project.edn writes

--- components/rpc/test/psi/rpc_test.clj
+++ components/rpc/test/psi/rpc_test.clj
@@ -89,7 +89,7 @@
   ([]
    (create-session-context {}))
   ([opts]
-   (let [ctx (session/create-context opts)
+   (let [ctx (session/create-context (test-support/safe-context-opts opts))
          sd  (session/new-session-in! ctx nil {})]
      [ctx (:session-id sd)])))
 
